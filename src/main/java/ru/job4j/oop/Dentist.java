7d68021006ac7c2e891427b729b56c4467414e4b
KirillReal <kiril5471710@gmail.com>
2020-10-03 15:33:02 +0300

    3. Реализация профессий в коде [#348433]

--- src/main/java/ru/job4j/oop/Dentist.java
+++ src/main/java/ru/job4j/oop/Dentist.java
@@ -2,7 +2,7 @@ package ru.job4j.oop;
 
 public class Dentist extends Doctor {
     private int numberOfInstruments;
-    public int PatientTeeth;
+    private int patientTeeth;
     public boolean drill(int patientTeeth){
         return Boolean.parseBoolean(null);
     }
