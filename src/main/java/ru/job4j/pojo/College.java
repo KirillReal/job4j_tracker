3616ad51f184b4a6cbbf5ad4d408995bb9f58c82
KirillReal <kiril5471710@gmail.com>
2020-10-06 01:14:46 +0300

    2. Модель данных. [#348435]

---
+++ src/main/java/ru/job4j/pojo/College.java
@@ -0,0 +1,11 @@
+package ru.job4j.pojo;
+import java.util.Date;
+public class College {
+    public static void main(String[] args) {
+        Student student = new Student();
+        student.setName("Danilov Kirill Andreevich");
+        student.setNumberOfGroup("381802");
+        student.setReceiptDate(new Date());
+        System.out.println("Student:" + student.getName() + System.lineSeparator() + student.getNumberOfGroup() + System.lineSeparator() + student.getReceiptDate());
+    }
+}
---
+++ src/main/java/ru/job4j/pojo/Student.java
@@ -0,0 +1,28 @@
+package ru.job4j.pojo;
+
+import java.util.Date;
+
+public class Student {
+    private String name;
+    private String numberOfGroup;
+    private Date receiptDate;
+    public String getName() {
+        return name;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public Date getReceiptDate() {
+        return receiptDate;
+    }
+    public void setReceiptDate(Date receiptDate) {
+        this.receiptDate = receiptDate;
+    }
+    public String getNumberOfGroup() {
+        return numberOfGroup;
+    }
+    public void setNumberOfGroup(String numberOfGroup) {
+        this.numberOfGroup = numberOfGroup;
+    }
+
+}
