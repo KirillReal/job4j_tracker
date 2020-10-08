d73a653dcfec07296e825397943d34d51a4f68e8
KirillReal <kiril5471710@gmail.com>
2020-10-08 03:43:32 +0300

    5. Tracker - хранилище [#348443]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -2,9 +2,18 @@ package ru.job4j.tracker;
 import javax.imageio.ImageTranscoder;
 import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
+import java.util.Arrays;
+
 public class StartUI {
     public static void main(String[] args) {
-        Item obj = new Item(1,"John");
-        System.out.println(obj);
+        Tracker obj = new Tracker();
+        Item first = new Item(1, "Sergey");
+        Item second = new Item(2,"Nikolay");
+        Item third = new Item(3,"Oleg");
+        obj.add(first);
+        obj.add(second);
+        obj.add(third);
+        Item check = obj.findById(1);
+        System.out.println(check);
     }
 }
--- src/main/java/ru/job4j/tracker/Tracker.java
+++ src/main/java/ru/job4j/tracker/Tracker.java
@@ -1,5 +1,7 @@
 package ru.job4j.tracker;
 
+import java.util.Arrays;
+
 public class Tracker {
     private final Item[] items = new Item[100];
     private int ids = 1;
@@ -10,7 +12,31 @@ public class Tracker {
         items[size++] = item;
         return item;
     }
-
+    public Item[] findAll(){
+       Item[] itemsWithoutNull = new Item[items.length];
+       int size = 0;
+       for(int i = 0; i < items.length;i++){
+           Item itemW = items[i];
+           if(itemW != null){
+               itemsWithoutNull[size] = itemW;
+               size++;
+           }
+           itemsWithoutNull = Arrays.copyOf(itemsWithoutNull, size);
+       }
+       return itemsWithoutNull;
+    }
+    public Item[] findByName(String key){
+        int size = 0;
+        Item[] equalName = new Item[items.length];
+        for (int index = 0; index < items.length; index++) {
+            Item name = items[index];
+            if (name.getName().equals(key)) {
+                equalName[size] = name;
+                size++;
+            }
+        }
+        return equalName;
+    }
     public Item findById(int id) {
         Item rsl = null;
         for (int index = 0; index < size; index++) {
