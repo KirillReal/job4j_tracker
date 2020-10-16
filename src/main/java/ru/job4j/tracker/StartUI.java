4e717b82b84c92063a2060cd86bf6f18317141c2
KirillReal <kiril5471710@gmail.com>
2020-10-17 00:13:54 +0300

    8. Реализация меню за счет шаблона стратегия. [#348370]

--- src/main/java/ru/job4j/tracker/StartUI.java
+++ src/main/java/ru/job4j/tracker/StartUI.java
@@ -25,7 +25,13 @@ public class StartUI {
         Input input = new ConsoleInput();
         Tracker tracker = new Tracker();
         UserAction[] actions = {
-                new CreateAction()
+                new CreateAction(),
+                new ShowAction(),
+                new ReplaceAction(),
+                new DeleteAction(),
+                new FindById(),
+                new FindItemsByName(),
+                new Exit(),
         };
         new StartUI().init(input, tracker, actions);
     }
