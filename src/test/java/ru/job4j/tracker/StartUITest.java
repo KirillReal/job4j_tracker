0792ebbebd2117b4cfc8de5dc543617248738b3f
KirillReal <kiril5471710@gmail.com>
2020-10-17 01:20:13 +0300

    9. Написать тесты на StartUI. [#348375]

--- src/test/java/ru/job4j/tracker/StartUITest.java
+++ src/test/java/ru/job4j/tracker/StartUITest.java
@@ -26,16 +26,17 @@ public class StartUITest {
         Item item = tracker.add(new Item("Replaced item"));
         /* Входные данные должны содержать ID добавленной заявки item.getId() */
         Item item2 = tracker.add(new Item("New item name"));
-        String replacedName = String.valueOf(item2);
+        String replacedName = item2.getName();
         Input in = new StubInput(
-                new String[] {"0", String.valueOf(item2.getId()) , "1"}
+                new String[] {"0", String.valueOf(item2.getId()), item2.getName(), "1"}
         );
         UserAction[] actions = {
                 new ReplaceAction(),
                 new Exit()
         };
         new StartUI().init(in, tracker, actions);
-        assertThat(tracker.findById(item.getId()), is(replacedName));
+        Item replaced = tracker.findById(item2.getId());
+        assertThat(replaced.getName(), is(replacedName));
     }
     @Test
     public void whenDeleteItem() {
@@ -44,7 +45,7 @@ public class StartUITest {
         Item item = tracker.add(new Item("Deleted item"));
         /* Входные данные должны содержать ID добавленной заявки item.getId() */
         Input in = new StubInput(
-                new String[] {"0" ,String.valueOf(item.getId()), "1"}
+                new String[] {"0" ,String.valueOf(item.getId()),"1"}
         );
         UserAction[] actions = {
                 new DeleteAction(),
