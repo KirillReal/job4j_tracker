7b945eba9834fd90877e826ee6831e4118808cb9
KirillReal <kiril5471710@gmail.com>
2020-10-30 02:37:46 +0300

    0. Map, HashMap [#348349]

---
+++ src/main/java/ru/job4j/collection/UsageMap.java
@@ -0,0 +1,14 @@
+package ru.job4j.collection;
+
+import java.util.HashMap;
+public class UsageMap {
+    public static void main(String[] args) {
+        HashMap<String,String> map = new HashMap<>();
+        map.put("kirill2341@yandex.ru", "Kirill Danilov");
+        map.put("sergeybron35421@gmail.com","Sergey Bronnikov");
+        for (String key : map.keySet()) {
+            String value = map.get(key);
+            System.out.println(key + " = " + value);
+        }
+    }
+}
