e1e417e3869d67520e91f26ec987b996af78d500
KirillReal <kiril5471710@gmail.com>
2020-11-03 02:58:34 +0300

    2. Комбинированный компаратор [#348338]

---
+++ src/test/java/ru/job4j/collection/JobTest.java
@@ -0,0 +1,65 @@
+package ru.job4j.collection;
+
+import org.junit.Test;
+import java.util.Comparator;
+import static org.hamcrest.Matchers.lessThan;
+import static org.hamcrest.Matchers.greaterThan;
+import static org.junit.Assert.assertThat;
+
+public class JobTest {
+    @Test
+    public void whenComparatorByNameAndPriority() {
+        Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobDescByPriority());
+        int rsl = cmpNamePriority.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl, lessThan(0));
+    }
+    @Test
+    public void whenComparatorByNameAndPriorityReverse2() {
+        Comparator<Job> cmpNamePriority = new JobDescByName().thenComparing(new JobDescByPriority());
+        int rsl = cmpNamePriority.compare(
+                new Job("Fix bug", 1),
+                new Job("Impl task", 0)
+        );
+        assertThat(rsl, greaterThan(0));
+    }
+    @Test
+    public void whenNameComparator() {
+        Comparator<Job> cmpName = new JobDescByName();
+        int rsl = cmpName.compare(
+                new Job("Fix bug", 1),
+                new Job("Impl task", 0)
+        );
+        assertThat(rsl,greaterThan(0));
+    }
+    @Test
+    public void whenNameComparatorReverse() {
+        Comparator<Job> cmpNameReverse = new JobDescByNameReverse();
+        int rsl = cmpNameReverse.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl,greaterThan(0));
+    }
+    @Test
+    public void whenPriority() {
+        Comparator<Job> cmpPriority = new JobDescByPriority();
+        int rsl = cmpPriority.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl,greaterThan(0));
+    }
+    @Test
+    public void whenPriorityReverse() {
+        Comparator<Job> cmpPriority = new JobDescByPriorityReverse();
+        int rsl = cmpPriority.compare(
+                new Job("Impl task", 0),
+                new Job("Fix bug", 1)
+        );
+        assertThat(rsl,greaterThan(0));
+    }
+}
+
