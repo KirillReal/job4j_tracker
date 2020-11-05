077b5ef3a1fff72a1cc3639ccbf420b51f303e1e
KirillReal <kiril5471710@gmail.com>
2020-11-06 01:04:49 +0300

    4. Сортировка номера [#348336]

--- src/main/java/ru/job4j/collection/LexSort.java
+++ src/main/java/ru/job4j/collection/LexSort.java
@@ -12,14 +12,11 @@ public class LexSort implements Comparator<String> {
         int sort = 0;
         List<String> list1 = Arrays.asList(left.split("\\ "));
         List<String> list2 = Arrays.asList(right.split("\\ "));
-        int i = 0;
-        do{
-            int listOne = Integer.parseInt(list1.get(i).substring(0,list1.get(i).length()-1));
-            int listTwo = Integer.parseInt(list2.get(i).substring(0,list2.get(i).length()-1));
-            sort = Integer.compare(listOne,listTwo);
-            i++;
-        }while(i < 1);
-
+        for(int i = 0; i < 1;i++) {
+            int listOne = Integer.parseInt(list1.get(i).substring(0, list1.get(i).length() - 1));
+            int listTwo = Integer.parseInt(list2.get(i).substring(0, list2.get(i).length() - 1));
+            sort = Integer.compare(listOne, listTwo);
+        }
         return sort;
     }
 }
