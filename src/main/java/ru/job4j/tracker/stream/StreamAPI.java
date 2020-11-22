74f812cef180f2c5f1cabc699b9a9ca686576148
KirillReal <kiril5471710@gmail.com>
2020-11-22 14:05:14 +0300

    0. Stream API [#348178]

--- src/main/java/ru/job4j/tracker/stream/StreamAPI.java
+++ src/main/java/ru/job4j/tracker/stream/StreamAPI.java
@@ -6,9 +6,14 @@ import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
 public class StreamAPI {
+
     public static void main(String[] args) {
-        List<Integer> integers = new ArrayList<>();
-        List<Integer> integersOfPositive = integers.stream().filter(
+        List<Integer> numList = new ArrayList<>();
+        numList.add(10);
+        numList.add(-12);
+        numList.add(21);
+        List<Integer> numOfPositive = numList.stream().filter(
                 num -> num > 0).collect(Collectors.toList());
+        numOfPositive.forEach(System.out::println);
     }
 }
