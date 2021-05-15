dd3a07dadc00b65240a85496cc042aa7cf5ea25d
KirillReal <kiril5471710@gmail.com>
2021-05-16 00:39:19 +0300

    2. Сканирование проекта [#490645]

--- src/main/java/ru/job4j/tracker/ConsoleInput.java
+++ src/main/java/ru/job4j/tracker/ConsoleInput.java
@@ -4,7 +4,7 @@ import org.springframework.stereotype.Component;
 
 import java.util.Scanner;
 
-
+@Component
 public class ConsoleInput implements Input {
     private Scanner scanner = new Scanner(System.in);
 
